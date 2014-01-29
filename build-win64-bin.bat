a5ccb7639583911331df652d2e99d1d5d1956d74
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2014-01-29 10:18:48 +0100

    build scripts updated

--- build-win64-bin.bat
+++ build-win64-bin.bat
@@ -28,7 +28,7 @@ CALL git describe --tags > build.info
 for /F "delims=\" %%a in (build.info) do (
 	set temp=%%a
 )
-SET mver="1.5.0"
+SET mver="1.5.0pre"
 pause
 
 makensis /Dmver=%mver% win-installer-64.nsi
